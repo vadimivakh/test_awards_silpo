f770a10bd224d9a625c84fae3f3dabdb49e5a0f8
Vadim Ivakh <ivakhvadim@gmail.com>
2018-08-27 12:46:17 +0300

    first commit

---
+++ mainpage.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+class MainPage:
+
+    def voting(self):
+        pass
+
+    def click_to_voting_topmenu_item(self):
+        pass
+
+    def click_to_about_project_topmenu_item(self):
+        pass
+
+    def click_to_rules_topmenu_item(self):
+        pass
+
+    def click_to_fag_topmenu_item(self):
+        pass
+
+    def click_to_my_choice_topmenu_item(self):
+        pass
+
+    def click_to_login_section(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_mainpage.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import mainpage
+
+
+class TestMainPage:
+
+    def test_mainpage(self):
+        pass
